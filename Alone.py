0e05b3d1d7be6bf9f5f72fc739fc4c0faf7ec46a
Mùghãl Zãdá <136257548+Tamoor12@users.noreply.github.com>
2024-02-05 14:06:31 +0500

    Update Alone.py
    Signed-off-by: Mùghãl Zãdá <136257548+Tamoor12@users.noreply.github.com>

--- Alone.py
+++ Alone.py
@@ -1,8 +1,1 @@
-import os, platform, time, sys
-try:
- import requests
-except:os.system("pip uninstall requests -y;pip install requests")
-
-print('\033[1;91m[\033[1;97m-\033[1;91m] \033[1;97mChecking For Update...')
-os.system('git pull --quiet 2>/dev/null')
  import abc
