25aac17afcd1d7ef81b60ca5f7e205e538c8cfd8
Mùghãl Zãdá <136257548+Tamoor12@users.noreply.github.com>
2024-02-05 14:13:53 +0500

    Add files via upload
    Signed-off-by: Mùghãl Zãdá <136257548+Tamoor12@users.noreply.github.com>

--- Alone.py
+++ Alone.py
@@ -1,1 +1,6 @@
- import abc
+import os
+#os.system("xdg-open https://chat.whatsapp.com/Dr8gnWcD7WdK6FKONbXyOu")
+try:import abc
+except Exception as e:
+  if "32-bit" in str(e):
+    print("[×] 32Bit Not Supported ")
